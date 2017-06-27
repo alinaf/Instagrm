4a806eef6aefc57f44708870ae7076a0ecc783af
Mei-Ling Laures <mlaures@fb.com>
2017-06-27 14:07:18 -0700

    configured Parse, connected to the correct server

--- Instagrm/AppDelegate.swift
+++ Instagrm/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,7 +16,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        // Initialize Parse -> Set applicationId and server based on Heroku settings
+        // clientKey not used in Parse open source unless specifically configured
+        Parse.initialize(with: ParseClientConfiguration(block: { (configuration: ParseMutableClientConfiguration) in
+            configuration.applicationId = "Instagrm"
+            configuration.clientKey = "InstaLookAlike"
+            configuration.server = "https://safe-journey-37542.herokuapp.com/parse"
+        }))
         return true
     }
 

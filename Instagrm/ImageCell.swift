95b07fbc47eed12055b051f4691387a1b28f6a2d
Mei-Ling Laures <mlaures@fb.com>
2017-06-27 15:56:41 -0700

    changed permissions for camera, photo library, https exceptions

---
+++ Instagrm/ImageCell.swift
@@ -0,0 +1,24 @@
+//
+//  ImageCell.swift
+//  Instagrm
+//
+//  Created by Mei-Ling Laures on 6/27/17.
+//  Copyright © 2017 Mei-Ling Laures. All rights reserved.
+//
+
+import UIKit
+
+class ImageCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

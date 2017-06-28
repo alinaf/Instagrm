32afee149a1adf9f69fe90a17fddc82d81402711
Mei-Ling Laures <mlaures@fb.com>
2017-06-28 11:57:41 -0700

    added created at label for header and fixed outlets

---
+++ Instagrm/UserSectionHeader.swift
@@ -0,0 +1,21 @@
+//
+//  UserSectionHeader.swift
+//  Instagrm
+//
+//  Created by Mei-Ling Laures on 6/27/17.
+//  Copyright © 2017 Mei-Ling Laures. All rights reserved.
+//
+
+import UIKit
+
+class UserSectionHeader: UITableViewHeaderFooterView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}

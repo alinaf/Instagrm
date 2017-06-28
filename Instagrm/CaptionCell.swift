6011dbcacdb9d22b588f95a6c35647d68a2405c0
Mei-Ling Laures <mlaures@fb.com>
2017-06-28 14:22:08 -0700

    made caption cell for the feed

---
+++ Instagrm/CaptionCell.swift
@@ -0,0 +1,28 @@
+//
+//  CaptionCell.swift
+//  Instagrm
+//
+//  Created by Mei-Ling Laures on 6/28/17.
+//  Copyright © 2017 Mei-Ling Laures. All rights reserved.
+//
+
+import UIKit
+
+class CaptionCell: UITableViewCell {
+
+    @IBOutlet weak var likesLabel: UILabel!
+    @IBOutlet weak var commentsLabel: UILabel!
+    @IBOutlet weak var captionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

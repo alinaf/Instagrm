df7a613ac5d32bf443e8948904cf169ca3334cf6
Mei-Ling Laures <mlaures@fb.com>
2017-06-28 13:16:29 -0700

    changed the cell/section headers to use in the feed table

--- Instagrm/ImageCell.swift
+++ Instagrm/ImageCell.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ImageCell: UITableViewCell {
 
+    @IBOutlet weak var postImage: UIImageView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
